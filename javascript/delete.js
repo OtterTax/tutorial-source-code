2fab59de684512d13a8a5253783b280122f8e6b8
OtterTax <affiliates@ottertax.com>
2021-08-25 12:21:59 -0400

    Make minor changes to comments.

--- javascript/delete.js
+++ javascript/delete.js
@@ -1,23 +1,21 @@
-//This import increases functionality to interact with the file system
+// Import fs so we can read and write files.
 import fs from 'fs';
 
-//import our team's helper methods from the file helpers.js
+// Import global helper methods
 import { getCredential, getGraphqlEndpoint, getData, toGqlObject, deleteCredentials } from './helpers.js'
 
-/**
+/*
  * Using graphql-request from
  * https://github.com/prisma-labs/graphql-request
  * Example tested with node version 14.16.0
  */
 import { GraphQLClient, gql } from 'graphql-request'
 
-
 /**
  * Build the GraphQL mutation for deleting statements.
- * @param {array} corrections An array of Uploader ID's of statements to dlete
+ * @param {array} uploaderIds An array of Uploader IDs of statements to delete.
  * @return {string} The full GraphQL delete mutation.
  */
-
 function buildMutation(uploaderIds) {
   const data = `
     mutation {
@@ -32,36 +30,32 @@ function buildMutation(uploaderIds) {
   return(data);
 }
 
-
 /**
  * Perform the work to get the access credentials.
- * format the IDs into GraphQL query syntax
- * and then send it to the server to delete the statements.
- * Return the confirmation data to give user success or failiure update.
+ * Format the IDs into GraphQL syntax, send the mutation to the server
+ * to delete the statements, and display the result for the user to review.
  */
-
 async function main() {
-  
   const credential = await getCredential();
   const endpoint = getGraphqlEndpoint();
   var uploaderIds = ['23913'];
   const graphQLClient = new GraphQLClient(endpoint, { headers: credential });
   const mutation = gql`${buildMutation(uploaderIds)}`;
   const response = await graphQLClient.request(mutation);
- 
-  /**
+
+  /*
    * Print out of server response of conformation that data was deleted,
-   * or errors if any occured
+   * or errors if any occured.
    */
   console.log(JSON.stringify(response,' ', '  '));
 
-  /**
-   * Delete User Credentials: delete the access keys from the server, this is
-   * good security practice to limit access to the user's Tax files
+  /*
+   * Credentials are valid for several days after they've been issued.
+   * If you plan to do more work, you can use the same credential.  When done 
+   * working, you should delete the credential to provide additional security.
    */
   await deleteCredentials(credential);
   console.log('Credentials Successfully Deleted')
 }
 
 main().catch((error) => console.error(error));
-
